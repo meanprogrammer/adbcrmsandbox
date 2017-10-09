eaa0839239d78556f5c6296bec0ce4e3b3045867
meanprogrammer <mean.programmer.d@gmail.com>
2017-10-09 16:50:58 +0800

    initial

---
+++ AdbCRMSandboxSLN/CRMSandboxService/AccountProxy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRMSandboxService
+{
+    public class AccountProxy
+    {
+        public string AccountNumber { get; set; }
+        public string LegalEntityIdentifier { get; set; }
+        public string AccountName { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Website { get; set; }
+    }
+}
---
+++ AdbCRMSandboxSLN/CRMSandboxService/ObjectConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRMSandboxService
+{
+    public static class ObjectConverter
+    {
+        public static List<AccountProxy> ConvertToListOfAccount(IEnumerable<Account> accounts, SandboxCRMContext context)
+        {
+            List<AccountProxy> list = new List<AccountProxy>();
+
+            foreach (Account a in accounts)
+            {
+                AccountProxy ap = ConvertToSingleAccount(a);
+                list.Add(ap);
+            }
+            return list;
+        }
+
+        public static AccountProxy ConvertToSingleAccount(Account account)
+        {
+            AccountProxy proxyObj = new AccountProxy();
+
+            proxyObj.AccountName = account.Name;
+            proxyObj.AccountNumber = account.AccountNumber;
+            proxyObj.Fax = account.Fax;
+            proxyObj.Phone = "no phone";
+            proxyObj.LegalEntityIdentifier = account.new_LEI;
+            proxyObj.Website = account.WebSiteURL;
+
+            return proxyObj;
+        }
+    }
+}
